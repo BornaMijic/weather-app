449c6a70b576be7e7acdfa11e8ad6e8574eefada
Borna Mijic <borna.mijic0309@gmail.com>
2022-05-11 16:04:43 +0200

    add login form and validation for login

---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {NgForm} from "@angular/forms";
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.css']
+})
+export class AuthComponent {
+  error: string = "";
+
+  constructor() { }
+
+  onSubmit(authForm: NgForm) {
+
+  }
+
+}
