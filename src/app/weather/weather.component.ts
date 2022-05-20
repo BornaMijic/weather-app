3723cca1f415507f475569c3d740013962650978
Borna Mijic <borna.mijic0309@gmail.com>
2022-05-20 22:22:37 +0200

    show feather data

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from "@angular/forms";
+import {Subscription} from "rxjs";
+import {WeatherService} from "./weather.service";
+import {Weather} from "./weather.model";
 
 @Component({
   selector: 'app-weather',
@@ -6,7 +10,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weather.component.css']
 })
 export class WeatherComponent {
-    cities: String[] = [];
-    cityName: string = "";
+    weathers: Weather[] = [];
     error: String = "";
+    subscription: Subscription = new Subscription();
+
+    constructor(private weatherService: WeatherService) {
+    }
+
+  addCity(cityWeatherForm: NgForm) {
+    const cityName: string = cityWeatherForm.value.cityName
+    this.subscription = this.weatherService.getCityWeather(cityName).subscribe(
+      resData =>{
+        if(resData != null) {
+          // @ts-ignore
+          const temp = resData.main.temp
+          // @ts-ignore
+          const icon = `http://openweathermap.org/img/w/${resData.weather[0].icon}.png`;
+          const weather: Weather = new Weather(cityName, temp, icon);
+          // @ts-ignore
+          console.log(resData.weather)
+          this.weathers.push(weather);
+          this.error = "";
+          cityWeatherForm.reset()
+        }
+      },
+      error => {
+        this.error = "There is no city with that name";
+        cityWeatherForm.reset()
+      }
+    );
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,9 @@
 
 export const environment = {
   production: false,
-  signInUrl: "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCfgBbiH9i6-3Lg22h47RNhyWZegGlyZjk"
+  signInUrl: "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCfgBbiH9i6-3Lg22h47RNhyWZegGlyZjk",
+  openWeatherSpecificCityUrl: "https://api.openweathermap.org/data/2.5/weather?q=",
+  apiKey: "1eafa79098796a04151a38fb2c7f2499"
 };
 
 /*
